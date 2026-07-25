96a419d4967df75234bc76965ac6107ad593326d
Sedem <s.amuzu@alustudent.com>
2026-07-25 02:01:33 -0700

    0-convolve_grayscale_valid.py

---
+++ 0-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    out_h = h - kh + 1
+    out_w = w - kw + 1
+
+    convolved = np.zeros((m, out_h, out_w))
+
+    for i in range(out_h):
+        for j in range(out_w):
+            image_slice = images[:, i:i + kh, j:j + kw]
+            convolved[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2))
+
+    return convolved
